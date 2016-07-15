70fbde81148f2302745832ac65f1b838196601e4
H. İbrahim Güngör <igungor@gmail.com>
2016-07-15 13:19:42 +0300

    transfers: more tests

--- putio/transfers_test.go
+++ putio/transfers_test.go
@@ -78,14 +78,85 @@ func TestTransfers_List(t *testing.T) {
 	setup()
 	defer teardown()
 
+	fixture := `
+{
+"status": "OK",
+"transfers": [
+	{
+	"availability": null,
+	"callback_url": null,
+	"client_ip": null,
+	"created_at": "2016-07-15T09:45:15",
+	"created_torrent": false,
+	"current_ratio": 0.00,
+	"down_speed": 0,
+	"download_id": 20448117,
+	"downloaded": 0,
+	"error_message": null,
+	"estimated_time": null,
+	"extract": false,
+	"file_id": 415107363,
+	"finished_at": "2016-07-15T09:45:16",
+	"id": 36003178,
+	"is_private": false,
+	"links": [
+		{
+			"label": "ud_logs",
+			"url": "https://papertrailapp.com/events?q=user_download_36003178"
+		},
+		{
+			"label": "d_logs",
+			"url": "https://papertrailapp.com/events?q=download_id_20448117"
+		},
+		{
+			"label": "torrent",
+			"url": "/v2/transfers/36003178/torrent"
+		},
+		{
+			"label": "magnet",
+			"url": "magnet:?xt=urn:btih:4344503b7e797ebf31582327a5baae35b11bda01&dn=ubuntu-16.04-desktop-amd64.iso"
+		}
+	],
+	"magneturi": "magnet:?xt=urn:btih:4344503b7e797ebf31582327a5baae35b11bda01&dn=ubuntu-16.04-desktop-amd64.iso",
+	"name": "ubuntu-16.04-desktop-amd64.iso",
+	"peers_connected": 0,
+	"peers_getting_from_us": 0,
+	"peers_sending_to_us": 0,
+	"percent_done": 100,
+	"save_parent_id": 400004368,
+	"seconds_seeding": 0,
+	"simulated": true,
+	"size": 1485881344,
+	"source": "http://releases.ubuntu.com/16.04/ubuntu-16.04-desktop-amd64.iso.torrent",
+	"status": "COMPLETED",
+	"status_message": "Completed 29 mins ago.",
+	"subscription_id": null,
+	"torrent_link": "/v2/transfers/36003178/torrent",
+	"tracker_message": null,
+	"trackers": null,
+	"type": "TORRENT",
+	"up_speed": 0,
+	"uploaded": 0
+}]
+}
+	`
+
 	mux.HandleFunc("/v2/transfers/list", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
-		fmt.Fprintln(w, `{"status":"OK", "transfers":[{"file_id": null,"name":"foo"}]}`)
+		fmt.Fprintln(w, fixture)
 	})
 
-	_, err := client.Transfers.List()
+	transfers, err := client.Transfers.List()
 	if err != nil {
 		t.Error(err)
+	}
+
+	if len(transfers) != 1 {
+		t.Errorf("got: %v, want: 1", len(transfers))
+	}
+
+	if transfers[0].ID != 36003178 {
+		t.Errorf("got: %v, want: 36003178", transfers[0].ID)
 	}
 }
 
