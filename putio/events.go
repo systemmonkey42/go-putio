9201102b92f5046f63e47c6bd6243cb82f26c8c4
H. İbrahim Güngör <igungor@gmail.com>
2016-07-18 12:04:32 +0300

    events: new service type

---
+++ putio/events.go
@@ -0,0 +1,52 @@
+package putio
+
+import "encoding/json"
+
+type EventsService struct {
+	client *Client
+}
+
+func (e *EventsService) List() ([]Event, error) {
+	req, err := e.client.NewRequest("GET", "/v2/events/list", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := e.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var r struct {
+		Status string
+		Events []Event
+	}
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		return nil, err
+	}
+
+	return r.Events, nil
+
+}
+
+func (e *EventsService) Delete() error {
+	req, err := e.client.NewRequest("POST", "/v2/events/delete", nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := e.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	var r struct {
+		Status string
+	}
+
+	// FIXME: return original error
+	return json.NewDecoder(resp.Body).Decode(&r)
+}
